b58dbfa7157d883efa3b7e9e16ea4125679c6959
oscar mwangangi <mwangangioscar2016@gmail.com>
2025-05-12 21:43:14 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text ('i am poor',
+        body: SafeArea(
+        child:  Container(
+          child: Column(
+            children:[
+              CircleAvatar(
+                  radius: 80,
+                  // backgroundColor: Colors.cyan,
+
+                  backgroundImage:  NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBcv-m3mLDcqJBDW1W05iZ4wX9CWUyvVCp7w&s')
+
+              ),
+              Text('Oscar Mwangangi',
+                  style: TextStyle(
+                      fontSize: 20,
+                    fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold
+
+              )
+              ),
+            Text('Oscar Mwangangi',
             style: TextStyle(
-              color: Colors.white
-            ),
-          ),
-          backgroundColor: Colors.black87,
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage('https://media.tenor.com/1Nv9azJcL5AAAAAM/emoji-laughing.gif'),
+                fontSize: 20,
+                fontFamily:'sans',
+                fontWeight: FontWeight.w100
+            )
+            )
+            ]
           )
+
         )
+
+        )
+
       ),
-    )
-);
+    );
+  }
+}
