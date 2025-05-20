bc2f789c088f9e0ce6e4db6fb8776fc381665ad5
oscar mwangangi <mwangangioscar2016@gmail.com>
2025-05-20 22:25:12 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,56 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-        child:  Container(
-          child: Column(
-            children:[
-              CircleAvatar(
-                  radius: 80,
-                  // backgroundColor: Colors.cyan,
-
-                  backgroundImage:  NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBcv-m3mLDcqJBDW1W05iZ4wX9CWUyvVCp7w&s')
-
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Profile Card'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Card(
+            elevation: 8,
+            margin: EdgeInsets.all(20),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBcv-m3mLDcqJBDW1W05iZ4wX9CWUyvVCp7w&s',
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Oscar Mwangangi',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Flutter Developer',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.phone, color: Colors.teal),
+                      SizedBox(width: 8),
+                      Text('+254 700 000 000'),
+                    ],
+                  ),
+                ],
               ),
-              Text('Oscar Mwangangi',
-                  style: TextStyle(
-                      fontSize: 20,
-                    fontFamily: 'Pacifico',
-                      fontWeight: FontWeight.bold
-
-              )
-              ),
-            Text('Oscar Mwangangi',
-            style: TextStyle(
-                fontSize: 20,
-                fontFamily:'sans',
-                fontWeight: FontWeight.w100
-            )
-            )
-            ]
-          )
-
-        )
-
-        )
-
+            ),
+          ),
+        ),
       ),
     );
   }
